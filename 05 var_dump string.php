b500e4b07dae258763c671308111ef43dfd295ab
daramaulida <daraksp291@gmail.com>
2024-08-29 10:46:42 +0700

    var_dump string

---
+++ 05 var_dump string.php
@@ -0,0 +1,11 @@
+<?php
+$variabel_string = "Halo Korea!";
+$variabel_integer = 123;
+$variabel_array = array(1, 2, "tiga", 4.5);
+
+var_dump($variabel_string);
+var_dump($variabel_integer);
+var_dump($variabel_array);
+
+echo "<br><br><strong>By:Dara Maulida";
+?>
