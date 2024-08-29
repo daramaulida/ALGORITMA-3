d438323485e6b5a6d98032a457705971ff767e9d
daramaulida <daraksp291@gmail.com>
2024-08-29 10:26:51 +0700

    print_r array

---
+++ 02 print_r array.php
@@ -0,0 +1,12 @@
+<?php
+
+//Contoh Penggunaan dengan Variabel Biasa
+$nama = "Dara<p>";
+print_r($nama);
+
+//Hasil:
+//Dara
+//Contoh Penggunaan dengan Array
+
+$minuman =array ("Teh","Kopi", "susu", "Air Mineral",);
+print_r($minuman);
