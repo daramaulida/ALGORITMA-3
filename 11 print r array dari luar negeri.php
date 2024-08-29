f2069f3a5d2af9affef6d585fac684dbc890c055
daramaulida <daraksp291@gmail.com>
2024-08-29 11:48:15 +0700

    print r array dari luar negeri

---
+++ 11 print r array dari luar negeri.php
@@ -0,0 +1,11 @@
+<?php
+
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+print_r($array);
+
+$array = '<p><strong>By:Dara Maulida';
+print_r($array);
