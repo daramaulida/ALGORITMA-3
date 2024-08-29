0eefd8cc5879d2f97627da523ad3ee3d5a006bbd
daramaulida <daraksp291@gmail.com>
2024-08-29 09:20:09 +0700

    variabel

---
+++ 01 variabel.php
@@ -0,0 +1,20 @@
+<?php
+
+$nama = 'Dara';
+echo "Halo, $nama!<br><br>"; // Hasil: Halo, Dara!
+
+//Menampilkan Variabel
+$umur = 16;
+echo "Umur saya adalah $umur tahun.<br><br>";
+
+//Menggabungkan Teks dan Variabel
+$depan = 'Dara';
+$belakang = 'Maulida';
+echo "Nama saya adalah " . $depan . " " . $belakang . ".<br><br>";
+
+//Menggunakan Echo untuk HTML
+echo "<h1>Judul Halaman</h1>";
+echo "<p>Paragraf ini dibuat dengan PHP.</p>";
+
+echo "<strong>By:Dara Maulida";
+
