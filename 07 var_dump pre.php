a49a4bfc338243597eff04136d1968fa26342add
daramaulida <daraksp291@gmail.com>
2024-08-29 10:57:58 +0700

    var_dump pre

---
+++ 07 var_dump pre.php
@@ -0,0 +1,8 @@
+<?php
+$variabel_array = array("Bolu Pisang", "Bolu coklat", "Bolu strawbarry");
+echo "<pre>";
+var_dump($variabel_array);
+echo "</pre>";
+
+echo "<br><strong>By:Dara Maulida";
+?>
