1b60f9e9c79731eccdd38a1aa4a87f73e56200de
daramaulida <daraksp291@gmail.com>
2024-08-29 11:02:35 +0700

    data boolean dan NULL

---
+++ 08 data boolean dan NULL.php
@@ -0,0 +1,13 @@
+<?php
+$var_boolean = true;
+$varboolean = false;
+$var_null = NULL;
+
+var_dump($var_boolean);
+print "<br><br>";
+var_dump($varboolean);
+print "<br><br>";
+var_dump($var_null);
+
+echo "<p><strong>By:Dara Maulida";
+?>
