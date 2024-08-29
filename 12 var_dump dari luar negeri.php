354c97209987b66690cf6abe56d62c6c1e838f20
daramaulida <daraksp291@gmail.com>
2024-08-29 11:51:02 +0700

    var_dump dari luar negeri

---
+++ 12 var_dump dari luar negeri.php
@@ -0,0 +1,12 @@
+<?php
+
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+ 
+$array = '<p><strong>By:Dara Maulida';
+print_r($array);
