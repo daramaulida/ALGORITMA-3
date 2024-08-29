148893b73ed33d17efca018b99f652d5a14cfc58
daramaulida <daraksp291@gmail.com>
2024-08-29 10:54:08 +0700

    var_dump objek

---
+++ 06 var_dump objek.php
@@ -0,0 +1,13 @@
+<?php
+class Bunga {
+    public $jenis;
+    public $warna;
+}
+
+$nama_bunga = new Bunga();
+$nama_bunga->jenis = "mawar ";
+$nama_bunga->warna = "merah";
+
+var_dump($nama_bunga);
+echo "<br><br><strong>By:Dara Maulida";
+?>
