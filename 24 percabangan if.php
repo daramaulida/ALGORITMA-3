e19b7e66db178a7064c7ae3c6538dd6655b49336
daramaulida <daraksp291@gmail.com>
2024-08-30 10:27:44 +0700

    percabangan if

---
+++ 24 percabangan if.php
@@ -0,0 +1,25 @@
+
+<?php
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah sebesar 55%!";
+    echo "<hr>";
+}
+
+$totalbelanja =180000;
+
+if($totalbelanja > 100000){
+    echo "Anda dapat belanja dengan nyaman menggunakan voucher
+    sebesar 200000!";
+    echo "<hr>";
+}
+
+$totalBelanja =190000;
+
+if($totalBelanja > 100000){
+    echo "Jika anda belanja buah di hari selasa maka akan 
+    dapat potongan harga RP 30000!";
+    echo "<hr>";
+}
+echo "<p><strong>By:Dara Maulida";
