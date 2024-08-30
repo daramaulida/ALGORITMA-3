d00b3457be3404ce89289bc6b517cf659809eab2
daramaulida <daraksp291@gmail.com>
2024-08-30 10:09:09 +0700

    operator numbers fungsi matematika lainnya

---
+++ 23 operator numbers fungsi matematika lainnya.php
@@ -0,0 +1,23 @@
+<?php
+
+//abs()
+//Menghitung nilai absolut atau nilai positif dari angka.
+echo abs(-4.2); // Hasil: 4.2
+echo "<hr>";
+
+//pow()
+//Menghitung nilai pangkat dari angka.
+echo pow(2, 4); // Hasil: 16
+echo "<hr>";
+
+//sqrt()
+//Menghitung akar kuadrat dari angka.
+echo sqrt(25); // Hasil: 5
+echo "<hr>";
+
+//max() dan min()
+//Mendapatkan nilai tertinggi dan terendah dari sejumlah angka atau array.
+echo max(2, 3, 1); // Hasil: 3
+echo min(array(2, 3, 1)); // Hasil: 1
+echo "<hr>";
+echo "<p><strong>By:Dara Maulida";
