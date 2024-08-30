708f741b34e961285bcd1da189b5b99434dee6dc
daramaulida <daraksp291@gmail.com>
2024-08-30 09:44:12 +0700

    operator bitwise

---
+++ 18 operator bitwise.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 70;
+$b = 15;
+
+// bitwise AND
+$c = $a & $b;
+echo "$a & $b = $c";
+echo "<br>";
+echo "<hr>";
+
+// bitwise OR
+$c = $a | $b;
+echo "$a | $b = $c";
+echo "<br>";
+echo "<hr>";
+
+// bitwise XOR
+$c = $a ^ $b;
+echo "$a ^ $b = $c";
+echo "<br>";
+echo "<hr>";
+
+// Shift Left
+$c = $a << $b;
+echo "$a << $b = $c";
+echo "<br>";
+echo "<hr>";
+
+// Shift Right
+$c = $a >> $b;
+echo "$a >> $b = $c";
+echo "<br>";
+echo "<hr>";
+
+echo "<p><strong>By:Dara Maulida";
