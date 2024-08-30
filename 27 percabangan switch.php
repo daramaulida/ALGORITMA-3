957b88607eca8045f4086de70d4477e7f8bc4b5b
daramaulida <daraksp291@gmail.com>
2024-08-30 10:51:19 +0700

    percabangan switch

---
+++ 27 percabangan switch.php
@@ -0,0 +1,120 @@
+<?php 
+
+$level = 1;
+
+switch($level){
+    case 1:
+        echo "Pelajari HTML";
+        echo "<hr>";
+        break;
+    case 2:
+        echo "Pelajari CSS";
+        break;
+    case 3:
+        echo "Pelajari Javascript";
+        echo "<hr>";
+        break;
+    case 4:
+        echo "Pelajari PHP";
+        break;
+    default:
+        echo "Kamu bukan programmer!";
+}
+
+$level = 2;
+
+switch($level){
+    case 1:
+        echo "Pelajari HTML";
+        echo "<hr>";
+        break;
+    case 2:
+        echo "Pelajari CSS";
+        echo "<hr>";
+        break;
+    case 3:
+        echo "Pelajari Javascript";
+        echo "<hr>";
+        break;
+    case 4:
+        echo "Pelajari PHP";
+        echo "<hr>";
+        break;
+    default:
+        echo "Kamu bukan programmer!";
+        echo "<hr>";
+}
+
+$level = 3;
+
+switch($level){
+    case 1:
+        echo "Pelajari HTML";
+        echo "<hr>";
+        break;
+    case 2:
+        echo "Pelajari CSS";
+        echo "<hr>";
+        break;
+    case 3:
+        echo "Pelajari Javascript";
+        echo "<hr>";
+        break;
+    case 4:
+        echo "Pelajari PHP";
+        echo "<hr>";
+        break;
+    default:
+        echo "Kamu bukan programmer!";
+        echo "<hr>";
+}
+
+$level = 4;
+
+switch($level){
+    case 1:
+        echo "Pelajari HTML";
+        echo "<hr>";
+        break;
+    case 2:
+        echo "Pelajari CSS";
+        echo "<hr>";
+        break;
+    case 3:
+        echo "Pelajari Javascript";
+        echo "<hr>";
+        break;
+    case 4:
+        echo "Pelajari PHP";
+        echo "<hr>";
+        break;
+    default:
+        echo "Kamu bukan programmer!";
+        echo "<hr>";
+}
+
+$level = 5;
+
+switch($level){
+    case 1:
+        echo "Pelajari HTML";
+        echo "<hr>";
+        break;
+    case 2:
+        echo "Pelajari CSS";
+        echo "<hr>";
+        break;
+    case 3:
+        echo "Pelajari Javascript";
+        echo "<hr>";
+        break;
+    case 4:
+        echo "Pelajari PHP";
+        echo "<hr>";
+        break;
+   case 5 :
+        echo "Kamu bukan programmer!";
+        echo "<hr>";
+}
+echo "<p><strong>By:Dara Maulida";
+?>
