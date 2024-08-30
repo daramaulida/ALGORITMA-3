7b792f4f9266d5b6afcc11b1701c61424234d40f
daramaulida <daraksp291@gmail.com>
2024-08-30 09:16:50 +0700

    operator penugasan

---
+++ 14 operator penugasan.php
@@ -0,0 +1,29 @@
+<?php
+
+$speed = 83;
+$speed2 = 50;
+$speed3 = 70;
+// ini opertor aritmatika
+$speed = $speed + 10;
+
+// maka nilai speed akan samadengan 83 + 10 = 93
+
+// ini operator penugasan
+$speed += 10;
+
+// sekarang nilai speed akan menjadi 93 + 10 = 103
+print ($speed)."<hr>";
+
+$speed2 = $speed2 +50;
+//dan
+$speed2 +=50;
+
+print ($speed2)."<hr>";
+
+$speed3 = $speed3 +70;
+//dan
+$speed3 +=70;
+
+print ($speed3)."<hr>";
+
+echo "<strong>By:Dara Maulida";
