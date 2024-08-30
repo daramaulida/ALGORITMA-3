d1aa4435d00aaffb896977dc58a93fe56c972713
daramaulida <daraksp291@gmail.com>
2024-08-30 09:31:53 +0700

    operator relasi

---
+++ 16 operator relasi.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 10;
+$b = 4;
+
+// menggunakan operator relasi lebih besar
+$c = $a > $b;
+echo "$a > $b: $c";
+echo "<hr>";
+
+// menggunakan operator relasi lebih kecil
+$c = $a < $b;
+echo "$a < $b: $c";
+echo "<hr>";
+
+// menggunakan operator relasi lebih sama dengan
+$c = $a == $b;
+echo "$a == $b: $c";
+echo "<hr>";
+
+// menggunakan operator relasi lebih tidak sama dengan
+$c = $a != $b;
+echo "$a != $b: $c";
+echo "<hr>";
+
+// menggunakan operator relasi lebih besar sama dengan
+$c = $a >= $b;
+echo "$a >= $b: $c";
+echo "<hr>";
+
+// menggunakan operator relasi lebih kecil sama dengan
+$c = $a <= $b;
+echo "$a <= $b: $c";
+echo "<hr>";
+
+echo "<p><strong>By:Dara Maulida";
