90261cbd31834ad6d40007110d4a9fa272f28e18
daramaulida <daraksp291@gmail.com>
2024-08-30 10:34:19 +0700

    percabangan if else

---
+++ 25 percabangan if else.php
@@ -0,0 +1,21 @@
+<?php
+
+$umur = 13;
+
+if ($umur < 18 ){
+    echo "Kamu tidak boleh membuka situs ini!";
+    echo "<hr>";
+} else {
+    echo "Selamat datang di website kami!";
+}
+
+$umur = 19;
+
+if ($umur < 18 ){
+    echo "<p>Kamu tidak boleh membuka situs ini!";
+} else {
+    echo "Selamat datang di website kami!";
+    echo "<hr>";
+}
+echo "<p><strong>By:Dara Maulida";
+?>
