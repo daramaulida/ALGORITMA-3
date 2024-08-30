36b250920519b6d3e4a5fe1069152283efdee865
daramaulida <daraksp291@gmail.com>
2024-08-30 10:05:03 +0700

    operator numbers operasi matematika dasar

---
+++ 22 operator numbers operasi matematika dasar.php
@@ -0,0 +1,23 @@
+<?php
+
+//Operasi Matematika Dasar
+//PHP memiliki operator matematika standar seperti penambahan, pengurangan, perkalian, dan pembagian.
+$angka1 = 20;
+$angka2 = 5;
+
+// Penambahan
+echo $angka1 + $angka2; // Hasil: 15
+echo "<hr>";
+
+// Pengurangan
+echo $angka1 - $angka2; // Hasil: 5
+echo "<hr>";
+
+// Perkalian
+echo $angka1 * $angka2; // Hasil: 50
+echo "<hr>";
+
+// Pembagian
+echo $angka1 / $angka2; // Hasil: 2
+echo "<hr>";
+echo "<p><strong>By:Dara Maulida";
