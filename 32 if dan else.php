a265c63fa301dc918575bf168a2ffe4482b5d903
daramaulida <daraksp291@gmail.com>
2024-08-30 11:37:23 +0700

    if dan else

---
+++ 32 if dan else.php
@@ -0,0 +1,32 @@
+<?php
+
+//Contoh Penggunaan If
+$umur = 20;
+
+if ($umur >= 17) {
+    echo "Kamu sudah cukup umur.";
+    echo "<hr>";
+}
+
+//Contoh Penggunaan If dan Else
+$umur = 16;
+
+if ($umur >= 17) {
+    echo "Kamu sudah cukup umur.";
+} else {
+    echo "Kamu belum cukup umur.";
+    echo "<hr>";
+}
+
+//Kombinasi Dengan ElseIf
+$suhu = 30;
+
+if ($suhu < 20) {
+    echo "Cuaca dingin.";
+} elseif ($suhu <= 30) {
+    echo "Cuaca hangat.";
+    echo "<hr>";
+} else {
+    echo "Cuaca panas.";
+}
+echo "<p><strong>By:Dara Maulida";
