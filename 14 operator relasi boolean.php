22e12ff1d623f516b29f15bbb4607318aa0fb9d0
daramaulida <daraksp291@gmail.com>
2024-08-30 09:37:44 +0700

    operator relasi boolean

---
+++ 14 operator relasi boolean.php
@@ -0,0 +1,15 @@
+<?php
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah!";
+}
+
+if($total_belanja < 100000){
+    echo "Belanja Sembako";
+}
+print("<hr>");
+echo("Belanja Sembako!");
+print("<hr>");
+
+echo ("<strong>By:DaraMaulida");
