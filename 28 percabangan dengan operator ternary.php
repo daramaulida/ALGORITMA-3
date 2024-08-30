a0a3de9bbb8601fa3e783a38f1c2c4a48ef3280a
daramaulida <daraksp291@gmail.com>
2024-08-30 10:55:48 +0700

    percabangan dengan operator ternary

---
+++ 28 percabangan dengan operator ternary.php
@@ -0,0 +1,11 @@
+<?php
+
+$suka = true;
+echo $suka ? "Dia juga suka kamu": "Baiklah!";
+echo "<hr>";
+
+$suka = false;
+echo $suka ? "Dia juga suka kamu": "Baiklah!";
+echo "<hr>";
+echo "<p><strong>By:Dara Maulida";
+?>
