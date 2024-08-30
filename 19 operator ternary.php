d4d49dd7369d79db72708b7b110782904c6c6138
daramaulida <daraksp291@gmail.com>
2024-08-30 09:49:53 +0700

    operator ternary

---
+++ 19 operator ternary.php
@@ -0,0 +1,13 @@
+<?php
+
+$suka = false;
+$suka2 =true;
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+$jawab2 = $suka2 ? "<p>iya": "tidak";
+// menampilkan jawaban
+echo $jawab;
+echo "<hr>";
+echo $jawab2;
+echo "<hr>";
+echo "<p><strong>By:Dara Maulida";
